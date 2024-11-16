a04c14ea61f561c891d0823beff24f8b5dd6f7a9
Rembrandt Kuipers <rem@kuipers.uk>
2024-11-16 12:13:29 +0700

    Struggling with Stylus internal issues, need to roll back

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -1,51 +1,32 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
+//! WIP changing from counter example to vote contract.
 
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use ethers::core::k256::sha2::digest::typenum::uint;
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::{StorageAddress, StorageMap, StorageU256, StorageVec}};
+use stylus_sdk::{alloy_primitives::*, prelude::*};
 
-#[storage]
-pub struct Votes {
-    owner: StorageAddress,
-    votes: StorageMap<U256, VoteRanking>,
+sol_storage! {
+    #[entrypoint]
+    pub struct Votes {
+        uint256 number;
+    }
 }
 
-#[storage]
-pub struct VoteRanking {
-    ranking: StorageVec<StorageU256>,
-}
-
-
+// #[storage]
+// pub struct Votes {
+//     // owner: StorageAddress,
+//     // votes: StorageMap<U256, StorageVec<StorageU256>>,
+// }
 
 #[public]
 impl Votes {
-    /// Gets the number from storage.
-    pub fn get_vote(&self) -> Vec<uint256> {
+    pub fn dummy(&self) -> U256 {
+        U256::from(0)
+    }
+
+       /// Gets the number from storage.
+       pub fn number(&self) -> U256 {
         self.number.get()
     }
 
@@ -69,4 +50,33 @@ impl Votes {
         let number = self.number.get();
         self.set_number(number + U256::from(1));
     }
+
+    // pub fn get_vote(&self, voter: U256) -> RustcDecodable<StorageVec<StorageU256>, RustcEncodable> {
+    //     self.votes.get(voter).into()
+    // }
+
+    // pub fn get_vote_len(&self, voter: U256) -> U256 {
+    //     self.votes.get(voter)
+    // }
+
+    // /// Sets a number in storage to a user-specified value.
+    // pub fn set_number(&mut self, new_number: U256) {
+    //     self.number.set(new_number);
+    // }
+
+    // /// Sets a number in storage to a user-specified value.
+    // pub fn mul_number(&mut self, new_number: U256) {
+    //     self.number.set(new_number * self.number.get());
+    // }
+
+    // /// Sets a number in storage to a user-specified value.
+    // pub fn add_number(&mut self, new_number: U256) {
+    //     self.number.set(new_number + self.number.get());
+    // }
+
+    // /// Increments `number` and updates its value in storage.
+    // pub fn increment(&mut self) {
+    //     let number = self.number.get();
+    //     self.set_number(number + U256::from(1));
+    // }
 }
