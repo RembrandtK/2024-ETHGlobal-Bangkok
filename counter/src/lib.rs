fb1605ad3c7bf66826e0531c1f5cad05507ed207
Rembrandt Kuipers <rem@kuipers.uk>
2024-11-16 13:56:07 +0700

    WIP

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -4,8 +4,6 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use std::{borrow::Borrow, ops::Deref};
-
 use stylus_sdk::{alloy_primitives::*, prelude::*, storage::*};
 
 // sol_storage! {
@@ -19,7 +17,7 @@ use stylus_sdk::{alloy_primitives::*, prelude::*, storage::*};
 pub struct Votes {
     number: StorageU256,
     // owner: StorageAddress,
-    votes: StorageMap<U256, StorageVec<StorageU256>>,
+    votes: StorageMap<VoterId, StorageVec<StorageU256>>,
 }
 
 pub type RankIndex = u32;
@@ -32,12 +30,16 @@ impl Votes {
         U256::from(0)
     }
 
-    pub fn rank_len(&self, voter: U256) -> RankIndex {
+    pub fn get_count(&self, voter: VoterId) -> RankIndex {
         self.votes.get(voter).len() as u32
     }
 
-    pub fn rank_item(&self, voter: U256, rank: RankIndex) {
+    pub fn get_candidate(&self, voter: VoterId, rank: RankIndex) -> CandidateId {
+        self.votes.get(voter).get(rank).unwrap_or_default()
+    }
 
+    pub fn push_candidate(&mut self, voter: VoterId, candidate: CandidateId) {
+        self.votes.setter(voter).push(candidate);
     }
 
     /// Gets the number from storage.
@@ -65,33 +67,4 @@ impl Votes {
         let number = self.number.get();
         self.set_number(number + U256::from(1));
     }
-
-    // pub fn get_vote(&self, voter: U256) -> RustcDecodable<StorageVec<StorageU256>, RustcEncodable> {
-    //     self.votes.get(voter).into()
-    // }
-
-    // pub fn get_vote_len(&self, voter: U256) -> U256 {
-    //     self.votes.get(voter)
-    // }
-
-    // /// Sets a number in storage to a user-specified value.
-    // pub fn set_number(&mut self, new_number: U256) {
-    //     self.number.set(new_number);
-    // }
-
-    // /// Sets a number in storage to a user-specified value.
-    // pub fn mul_number(&mut self, new_number: U256) {
-    //     self.number.set(new_number * self.number.get());
-    // }
-
-    // /// Sets a number in storage to a user-specified value.
-    // pub fn add_number(&mut self, new_number: U256) {
-    //     self.number.set(new_number + self.number.get());
-    // }
-
-    // /// Increments `number` and updates its value in storage.
-    // pub fn increment(&mut self) {
-    //     let number = self.number.get();
-    //     self.set_number(number + U256::from(1));
-    // }
 }
